efd86b0c7b472a97380fc6e50c0116672dbe489b
Vick Mu <arbi3400@gmail.com>
2024-02-26 13:04:04 -0500

    Create private note method

--- ca_api_wrapper/api/orders.py
+++ ca_api_wrapper/api/orders.py
@@ -1,4 +1,5 @@
 from .client import ChannelAdvisorClient
+import json
 
 class OrderClient:
     def __init__(self, client: ChannelAdvisorClient):
@@ -12,3 +13,6 @@ class OrderClient:
     
     def create(self, order):
         return self.client.make_request("v1/orders/Create", method="POST", data=order)
+    
+    def create_private_note(self, order, id:int, note:str): 
+        return self.client.make_request(f"v1/orders{id}", data=json.dumps({"PrivateNotes": {note}}))
