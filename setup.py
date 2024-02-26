9bee4436293d96378edb795831c44444f58a5692
Vick Mu <arbi3400@gmail.com>
2024-02-26 11:16:33 -0500

    readme.md for description

--- setup.py
+++ setup.py
@@ -1,5 +1,8 @@
 from setuptools import setup, find_packages
 
+with open('README.md', encoding='utf-8') as f:
+    long_description = f.read()
+
 setup(
     name='ca_api_wrapper',
     version='1.0.0',
@@ -9,8 +12,7 @@ setup(
     license='LICENSE',
     description='A NON-OFFICIAL Python package for accessing the ChannelAdvisor RESTful API',
     long_description=open('README.md').read(),
-    install_requires=[
-        # Your package dependencies
-    ],
+    long_description=long_description,
+    long_description_content_type='text/markdown',
 )
 
