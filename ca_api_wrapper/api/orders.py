63b04bac3bafeaf902013260282a97246c5cddc0
Vick Mu <arbi3400@gmail.com>
2024-02-26 14:08:17 -0500

    enabled pagination

--- ca_api_wrapper/api/orders.py
+++ ca_api_wrapper/api/orders.py
@@ -2,18 +2,35 @@ from .client import ChannelAdvisorClient
 from .filters import Filter
 import json
 
+from typing import List, Dict, Any
+
 class OrderClient:
     def __init__(self, client: ChannelAdvisorClient):
         self.client = client
 
-    def list(self, order_filter: Filter=None):
-        if order_filter: 
-            assert isinstance(type(order_filter) == Filter)
+    def list(self, order_filter: Filter = None, paginate: bool = False, page_size: int = 250) -> List[Dict[str, Any]]:
+        orders = []
+        params = {"$top": page_size}
+
+        if order_filter:
+            assert isinstance(order_filter, Filter), "order_filter must be a Filter object"
+            params["$filter"] = order_filter.get_filter()
+
+        next_link = "v1/orders"
         
-        params = {
-            "$filter": order_filter.get_filter()
-        }
-        return self.client.make_request("v1/orders", params=params)
+        while next_link:
+            response = self.client.make_request(next_link, params=params)
+            response_data = response.json()
+            orders.extend(response_data.get('value', []))  # Assuming the list of orders is under 'value'
+            
+            # Check if there's a next link and if pagination is requested
+            next_link = response_data.get('@odata.nextLink') if paginate else None
+            if next_link:
+                next_link = next_link.replace(self.client.base_url, "") 
+                # Prepare for the next request
+                params = {} 
+                
+        return orders
 
     def get_by_id(self, id):
         return self.client.make_request(f"v1/Orders({id})?$expand=Items")
