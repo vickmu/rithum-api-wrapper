72ce600b1bcb2f0dfa834b02a7f782de96fc544d
Vick Mu <arbi3400@gmail.com>
2024-02-25 22:47:07 -0500

    init

---
+++ ca_api_wrapper/api/client_registry.py
@@ -0,0 +1,26 @@
+from .client import ChannelAdvisorClient
+from .products import ProductClient
+from .orders import OrderClient
+from .exports import ProductExportClient
+
+class ClientsFactory:
+    def __init__(self, access_token: str, default_profile_id: str, secondary_profile_id: str = None, base_url: str = "https://api.channeladvisor.com") -> None:
+        self.access_token = access_token
+        self.default_profile_id = default_profile_id
+        self.secondary_profile_id = secondary_profile_id
+        self.base_url = base_url
+        
+        self._base_client = ChannelAdvisorClient(access_token, default_profile_id, secondary_profile_id, base_url)
+
+    @property
+    def product_client(self):
+        return ProductClient(self._base_client)
+
+    @property
+    def order_client(self):
+        return OrderClient(self._base_client)
+
+    @property
+    def export_client(self):
+        return ProductExportClient(self._base_client)
+
--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='ca-api-wrapper',
+    name='ca_api_wrapper',
     version='1.0.0',
     author='Vick Mu',
     author_email='arbi3400@gmail.com',
@@ -14,24 +14,3 @@ setup(
     ],
 )
 
-from setuptools import setup, find_packages
-
-setup(
-    name='channeladvisor',
-    version='0.1.0',
-    author='Your Name',
-    author_email='your.email@example.com',
-    packages=find_packages(),
-    install_requires=[
-        'requests>=2.25.1',
-        'pandas>=1.3.4',
-        'numpy>=1.21.5',
-    ],
-    long_description=open('README.md').read(),
-    url='https://github.com/vickmu/rithum-api-wrapper',  
-    classifiers=[
-        'Programming Language :: Python :: 3',
-        'License :: OSI Approved :: MIT License', 
-        'Operating System :: OS Independent',
-    ],
-)
